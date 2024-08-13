ba2a7f708f923f7a050bc0b4efbe6af26e3fb13e
Katerine18 <katerinecm2408@gmail.com>
2024-08-13 01:13:09 -0600

    creadr

---
+++ Assets/Scripts/CharacterListener.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CharacterListener : MonoBehaviour
+{
+    [SerializeField]
+    AttackMode[] attackModes;
+
+    private CharacterController2D _character;
+
+    private void Awake()
+    {
+        _character = GetComponentInParent<CharacterController2D>();
+    }
+
+    private AttackMode GetAttackMode(string name)
+    {
+        foreach (AttackMode attackMode in attackModes)
+        {
+            if (attackMode.getName().Equals(name, System.StringComparison.OrdinalIgnoreCase))
+            {
+                return attackMode;
+            }
+        }
+        return null;
+    }
+
+    public void OnPunch()
+    {
+        AttackMode attackMode = GetAttackMode("OnPunch");
+        _character.Punch(attackMode.getDamage(), attackMode.getIsPercentage());
+    }
+
+    public void OnSuper()
+    {
+        AttackMode attackMode = GetAttackMode("OnSuper");
+        _character.Super(attackMode.getDamage(), attackMode.getIsPercentage());
+    }
+}
